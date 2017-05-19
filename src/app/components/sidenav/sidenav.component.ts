3c352b0d20713274987bea36d41f16c8a0558c46
unknown <sxu@game.ntes>
2017-05-20 00:07:06 +0800

    navbar

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -2,19 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from './menuitem';
 // import { HeroService } from './hero.service';
 
-
-
 const MENULIST: MenuItem[] = [
-  { id: 11, name: '网络游戏' },
-  { id: 12, name: '单机游戏' },
-  { id: 13, name: '上传管理' },
+  { id: 11, name: '上传组件管理' },
+  { id: 12, name: '单机游戏管理' },
+  { id: 13, name: '网络游戏管理' },
   { id: 14, name: '收益管理' },
   { id: 15, name: '其他内容' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
 ];
 
 
